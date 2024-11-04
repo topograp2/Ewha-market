d34c590f1cf4042c2d4a46271aacbdf657e458d2
ganotyoung <rrddwf0224@naver.com>
2024-11-04 18:09:26 +0900

    list fix

--- static/main.js
+++ static/main.js
@@ -5,3 +5,21 @@ navLinks.forEach(link => {
     link.classList.add('active');
   }
 });
+
+const cateLinks = document.querySelectorAll('.category-nav a');
+cateLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    event.preventDefault();
+    cateLinks.forEach(link => link.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
+
+const pageLinks = document.querySelectorAll('.pagination a');
+pageLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    event.preventDefault();
+    pageLinks.forEach(link => link.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
