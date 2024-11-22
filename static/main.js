8a1e625313418a7ef3c38ba71c717b149e406292
ganotyoung <rrddwf0224@naver.com>
2024-11-22 17:01:10 +0900

    fix

--- static/main.js
+++ static/main.js
@@ -6,23 +6,37 @@ navLinks.forEach(link => {
   }
 });
 
-const cateLinks = document.querySelectorAll('.category-nav a');
-cateLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    event.preventDefault();
-    cateLinks.forEach(link => link.classList.remove('active'));
-    link.classList.add('active');
-    updateSubcategoryPosition();
+document.addEventListener('DOMContentLoaded', () => {
+  const categoryLinks = document.querySelectorAll('.category-nav a');
+  const subcategories = document.querySelectorAll('.subcategory-nav');
+
+  // 초기값: 첫 번째 카테고리와 서브카테고리 활성화
+  if (categoryLinks.length > 0 && subcategories.length > 0) {
+    categoryLinks[0].classList.add('active');
+    subcategories[0].classList.add('active');
+  }
+
+  categoryLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault(); // 기본 링크 동작 방지
+      const targetId = link.getAttribute('data-target');
+
+      // 모든 서브 카테고리를 숨김
+      subcategories.forEach(subcategory => {
+        subcategory.classList.remove('active');
+      });
+
+      // 모든 링크에서 active 제거
+      categoryLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+
+      // 선택된 서브 카테고리 표시 및 링크 강조
+      const targetSubcategory = document.getElementById(targetId);
+      if (targetSubcategory) {
+        targetSubcategory.classList.add('active');
+        link.classList.add('active');
+      }
+    });
   });
 });
-
-function updateSubcategoryPosition() {
-  const activeElement = document.querySelector('.category-nav a.active');
-  const subcategoryNav = document.getElementById('subcategory');
-
-  const parentRect = activeElement.parentElement.getBoundingClientRect();
-  const rect = activeElement.getBoundingClientRect();
-
-  subcategoryNav.style.marginLeft = `${rect.left - parentRect.left+150}px`;
-  subcategoryNav.style.visibility = 'visible';
-}
