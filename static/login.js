9d0b9adcaa0a21bcd1f6dd6cdfdfd4eaed0c2566
ganotyoung <rrddwf0224@naver.com>
2024-11-25 18:18:51 +0900

    fix

---
+++ static/login.js
@@ -0,0 +1,16 @@
+document.getElementById("pw-toggle-btn").addEventListener("click", function () {
+    const passwordInput = document.getElementById("password-input");
+    const pwIcon = document.getElementById("pw-icon");
+
+    if (passwordInput.type === "password") {
+        // 비밀번호 보이기
+        passwordInput.type = "text";
+        pwIcon.src = "../static/svg/pwShow.svg";
+        pwIcon.alt = "비밀번호 숨기기 버튼";
+    } else {
+        // 비밀번호 숨기기
+        passwordInput.type = "password";
+        pwIcon.src = "../static/svg/pwHide.svg";
+        pwIcon.alt = "비밀번호 보기 버튼";
+    }
+});
