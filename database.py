8f3273544f015ca2d4d95d9a99c258b84cb2be4b
jeein <23minjjj@ewha.ac.kr>
2024-11-25 20:42:24 +0900

    좋아요 관련 db.py 수정

--- database.py
+++ database.py
@@ -109,8 +109,8 @@ class DBhandler:
             return target_value
         for res in hearts.each():
             key_value = res.key()
-        if key_value == name:
-            target_value = res.val()
+            if key_value == name:
+                target_value = res.val()
         return target_value
     
     def update_heart(self, user_id, isHeart, item):
